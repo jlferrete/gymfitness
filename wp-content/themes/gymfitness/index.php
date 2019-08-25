e5d4d5bcdac4fc7735f9c3a16830d27f69511b11
JlFerrete <joseluisferrete@gmail.com>
2019-08-25 11:38:00 +0200

    Included basic header & footer

--- wp-content/themes/gymfitness/index.php
+++ wp-content/themes/gymfitness/index.php
@@ -1,1 +1,3 @@
+<?php get_header(); ?>
+
 <h1>Hola desde index.php</h1>
--- wp-content/themes/gymfitness/single.php
+++ wp-content/themes/gymfitness/single.php
@@ -1,1 +1,13 @@
-<h1>Hola desde single.php</h1>
+<?php get_header(); ?>
+
+<?php
+    while( have_posts() ): the_post();
+?>
+
+    <h1><?php the_title(); ?></h1>
+
+    <?php the_content(); ?>
+
+<?php endwhile; ?>
+
+<?php get_footer(); ?>
