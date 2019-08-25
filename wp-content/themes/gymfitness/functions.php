516bd3273a83a1123a7870b8b6426f3659d6e1f1
JlFerrete <joseluisferrete@gmail.com>
2019-08-25 13:05:50 +0200

    Added custom properties to css

--- wp-content/themes/gymfitness/functions.php
+++ wp-content/themes/gymfitness/functions.php
@@ -18,7 +18,7 @@ function gymfitness_scripts_styles() {
     wp_enqueue_style('googleFont', 'https://fonts.googleapis.com/css?family=Open+Sans|Raleway:400,700,900|Staatliches&display=swap', array(), '1.0.0');
     
     //Hoja de estilos principal
-    wp_enqueue_style('style', get_stylesheet_uri(), array('normalize, googleFont'), '1.0.0');
+    wp_enqueue_style('style', get_stylesheet_uri(), array('normalize', 'googleFont'), '1.0.0');
 }
 add_action('wp_enqueue_scripts', 'gymfitness_scripts_styles');
 
