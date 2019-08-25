c629cd4a04e49a3724db755675bb2713d033f542
JlFerrete <joseluisferrete@gmail.com>
2019-08-25 23:25:50 +0200

    Added navbar for mobile version

--- wp-content/themes/gymfitness/functions.php
+++ wp-content/themes/gymfitness/functions.php
@@ -13,12 +13,18 @@ function gymfitness_scripts_styles() {
     
     //Cargamos normalize
     wp_enqueue_style('normalize', get_template_directory_uri() . '/css/normalize.css', array(), '8.0.1');
-
+    
+    wp_enqueue_style('slicknavCSS', get_template_directory_uri() . '/css/slicknav.min.css', array(), '1.0.0');
 
     wp_enqueue_style('googleFont', 'https://fonts.googleapis.com/css?family=Open+Sans|Raleway:400,700,900|Staatliches&display=swap', array(), '1.0.0');
     
     //Hoja de estilos principal
     wp_enqueue_style('style', get_stylesheet_uri(), array('normalize', 'googleFont'), '1.0.0');
+
+    wp_enqueue_script('slicknavJS', get_template_directory_uri() . '/js/jquery.slicknav.min.js', array('jquery'), '1.0.0', true);
+
+    wp_enqueue_script('scripts', get_template_directory_uri() . '/js/scripts.js', array('jquery', 'slicknavJS'), '1.0.0', true);
+
 }
 add_action('wp_enqueue_scripts', 'gymfitness_scripts_styles');
 
---
+++ wp-content/themes/gymfitness/js/scripts.js
@@ -0,0 +1,8 @@
+jQuery(document).ready($ => {
+
+    $('.menu-principal .menu').slicknav({
+        label: '',
+        appendTo: '.site-header'
+    });
+
+});
