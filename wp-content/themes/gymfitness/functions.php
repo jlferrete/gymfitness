0b9ec5edb17b462d5cf79a19de235dc5ddf5119a
JlFerrete <joseluisferrete@gmail.com>
2019-08-28 22:35:48 +0200

    Registered some image sizes

--- wp-content/themes/gymfitness/functions.php
+++ wp-content/themes/gymfitness/functions.php
@@ -7,6 +7,13 @@ function gymfitness_setup() {
     // Habilitar imagenes destacadas
     add_theme_support('post-thumbnails');
 
+    //Agregar imagenes de tamaño personalizado
+    add_image_size( 'square', 350, 350, true );
+    add_image_size( 'portrait', 350, 724, true );
+    add_image_size( 'cajas', 400, 375, true );
+    add_image_size( 'mediano', 700, 400, true );
+    add_image_size( 'blog', 966, 644, true );
+
 }
 add_action('after_setup_theme', 'gymfitness_setup');
 
--- wp-content/themes/gymfitness/page.php
+++ wp-content/themes/gymfitness/page.php
@@ -7,7 +7,7 @@
     <h1><?php the_title(); ?></h1>
 
     <?php if( has_post_thumbnail() ) :
-            the_post_thumbnail(); 
+            the_post_thumbnail('blog'); 
         endif;
     ?>
 
