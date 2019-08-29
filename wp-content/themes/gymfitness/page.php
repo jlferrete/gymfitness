fd95b5647bd759bb14802e1f3b5b50c9be68e8d1
JlFerrete <joseluisferrete@gmail.com>
2019-08-29 23:39:45 +0200

    CSS to about page

--- wp-content/themes/gymfitness/page.php
+++ wp-content/themes/gymfitness/page.php
@@ -1,18 +1,26 @@
 <?php get_header(); ?>
 
-<?php
-    while( have_posts() ): the_post();
-?>
+    <main class="contenedor pagina seccion con-sidebar">
+        <div class="contenido-principal">
+            <?php
+                while( have_posts() ): the_post();
+            ?>
 
-    <h1><?php the_title(); ?></h1>
+                <h1 class="text-center texto-primario"><?php the_title(); ?></h1>
 
-    <?php if( has_post_thumbnail() ) :
-            the_post_thumbnail('blog'); 
-        endif;
-    ?>
+                <?php if( has_post_thumbnail() ) :
+                    the_post_thumbnail('blog', array('class' => 'imagen-destacada')); 
+                endif;
+                ?>
 
-    <?php the_content(); ?>
+                <?php the_content(); ?>
 
-<?php endwhile; ?>
+            <?php endwhile; ?>
+        </div>
+
+        <?php get_sidebar(); ?>
+
+
+    </main>
 
 <?php get_footer(); ?>
---
+++ wp-content/themes/gymfitness/sidebar.php
@@ -0,0 +1,3 @@
+<aside class="sidebar">
+    <h1 class=" text-center texto-primario">sidebar aqui</h1>
+</aside>
