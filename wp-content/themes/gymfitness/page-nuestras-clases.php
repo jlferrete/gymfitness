1920deb3a3cacca3540894d4380146e6b1ae698a
JlFerrete <joseluisferrete@gmail.com>
2019-08-30 23:08:06 +0200

    Created 'clases' template

---
+++ wp-content/themes/gymfitness/page-nuestras-clases.php
@@ -0,0 +1,9 @@
+<?php get_header(); ?>
+
+    <main class="contenedor pagina seccion no-sidebar">
+        <div class="contenido-principal text-center">
+            <?php get_template_part('template-parts/paginas'); ?>
+        </div>
+    </main>
+
+<?php get_footer(); ?>
