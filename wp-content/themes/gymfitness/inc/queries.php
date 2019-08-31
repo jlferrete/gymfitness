a04fbe279eae7f6d5d2715e3f2d516693047d909
JlFerrete <joseluisferrete@gmail.com>
2019-08-31 08:36:48 +0200

    Added cards into 'clases'

--- wp-content/themes/gymfitness/inc/queries.php
+++ wp-content/themes/gymfitness/inc/queries.php
@@ -13,8 +13,18 @@ function gymfitness_lista_clases() { ?>
     
             while( $clases->have_posts() ): $clases->the_post(); ?>
 
-            <li>
-                <h1><?php the_title(); ?></h1>
+            <li class="clase card">
+                <?php the_post_thumbnail('mediano'); ?>
+                <div class="contenido">
+                    <a href="<?php the_permalink(); ?>">
+                        <h3><?php the_title(); ?></h3>
+                    </a>
+                    <?php
+                        $hora_inicio = get_field('hora_inicio');
+                        $hora_fin = get_field('hora_fin');
+                    ?>
+                    <p><?php the_field('dias_clase'); ?> - <?php echo $hora_inicio . " a " . $hora_fin; ?></p>
+                </div>
             </li>
                 
             <?php endwhile; wp_reset_postdata(); ?>
--- wp-content/themes/gymfitness/page-nuestras-clases.php
+++ wp-content/themes/gymfitness/page-nuestras-clases.php
@@ -1,7 +1,7 @@
 <?php get_header(); ?>
 
     <main class="contenedor pagina seccion no-sidebar">
-        <div class="contenido-principal text-center">
+        <div class="text-center">
             <?php get_template_part('template-parts/paginas'); ?>
 
             <?php gymfitness_lista_clases(); ?>
