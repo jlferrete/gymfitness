dab44a9df0f681f0cbb2f927c770a8643203e93c
JlFerrete <joseluisferrete@gmail.com>
2019-09-01 02:03:55 +0200

    Improved blog structure & CSS

--- wp-content/themes/gymfitness/category.php
+++ wp-content/themes/gymfitness/category.php
@@ -1,11 +1,14 @@
 <?php get_header(); ?>
-
     <main class="pagina seccion no-sidebars contenedor">
+    
+    <?php 
+        $categoria = get_queried_object();
+    ?>
+
+        <h2 class="text-center texto-primario">Categoría: <?php echo $categoria->name ?></h2>
+
         <ul class="listado-blog">
             <?php get_template_part('template-parts/loop', 'blog'); ?>
         </ul>
     </main>
-
-
-
 <?php get_footer(); ?>
