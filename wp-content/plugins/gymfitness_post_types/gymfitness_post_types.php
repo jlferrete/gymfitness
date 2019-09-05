fe1804e6c90e4b7859ef6238f7081d5930794e53
JlFerrete <joseluisferrete@gmail.com>
2019-09-05 23:39:18 +0200

    Section 'Instructores'

--- wp-content/plugins/gymfitness_post_types/gymfitness_post_types.php
+++ wp-content/plugins/gymfitness_post_types/gymfitness_post_types.php
@@ -67,3 +67,59 @@ function gymfitness_clases_post_type() {
 
 }
 add_action( 'init', 'gymfitness_clases_post_type', 0 );
+
+// Register Custom Post Type
+function gymfitness_instructores() {
+
+	$labels = array(
+		'name'                  => _x( 'Instructores', 'Post Type General Name', 'gymfitness' ),
+		'singular_name'         => _x( 'Instructor', 'Post Type Singular Name', 'gymfitness' ),
+		'menu_name'             => __( 'Instructores', 'gymfitness' ),
+		'name_admin_bar'        => __( 'Instructor', 'gymfitness' ),
+		'archives'              => __( 'Archivo', 'gymfitness' ),
+		'attributes'            => __( 'Atributos', 'gymfitness' ),
+		'parent_item_colon'     => __( 'Instructor Padre', 'gymfitness' ),
+		'all_items'             => __( 'Todas Las Instructores', 'gymfitness' ),
+		'add_new_item'          => __( 'Agregar Instructor', 'gymfitness' ),
+		'add_new'               => __( 'Agregar Instructor', 'gymfitness' ),
+		'new_item'              => __( 'Nueva Instructor', 'gymfitness' ),
+		'edit_item'             => __( 'Editar Instructor', 'gymfitness' ),
+		'update_item'           => __( 'Actualizar Instructor', 'gymfitness' ),
+		'view_item'             => __( 'Ver Instructor', 'gymfitness' ),
+		'view_items'            => __( 'Ver Instructores', 'gymfitness' ),
+		'search_items'          => __( 'Buscar Instructor', 'gymfitness' ),
+		'not_found'             => __( 'No Encontrado', 'gymfitness' ),
+		'not_found_in_trash'    => __( 'No Encontrado en Papelera', 'gymfitness' ),
+		'featured_image'        => __( 'Imagen Destacada', 'gymfitness' ),
+		'set_featured_image'    => __( 'Guardar Imagen destacada', 'gymfitness' ),
+		'remove_featured_image' => __( 'Eliminar Imagen destacada', 'gymfitness' ),
+		'use_featured_image'    => __( 'Utilizar como Imagen Destacada', 'gymfitness' ),
+		'insert_into_item'      => __( 'Insertar en Instructor', 'gymfitness' ),
+		'uploaded_to_this_item' => __( 'Agregado en Instructor', 'gymfitness' ),
+		'items_list'            => __( 'Lista de Instructores', 'gymfitness' ),
+		'items_list_navigation' => __( 'Navegación de Instructores', 'gymfitness' ),
+		'filter_items_list'     => __( 'Filtrar Instructores', 'gymfitness' ),
+	);
+	$args = array(
+		'label'                 => __( 'Instructores', 'gymfitness' ),
+		'description'           => __( 'Instructores para el Sitio Web', 'gymfitness' ),
+		'labels'                => $labels,
+		'supports'              => array( 'title', 'editor', 'thumbnail' ),
+		'hierarchical'          => false,
+		'public'                => true,
+		'show_ui'               => true,
+		'show_in_menu'          => true,
+        'menu_position'         => 7,
+        'menu_icon'             => 'dashicons-admin-users',
+		'show_in_admin_bar'     => true,
+		'show_in_nav_menus'     => true,
+		'can_export'            => true,
+		'has_archive'           => false,
+		'exclude_from_search'   => false,
+		'publicly_queryable'    => true,
+		'capability_type'       => 'page',
+	);
+	register_post_type( 'instructores', $args );
+
+}
+add_action( 'init', 'gymfitness_instructores', 0 );
--- wp-content/themes/gymfitness/front-page.php
+++ wp-content/themes/gymfitness/front-page.php
@@ -57,6 +57,43 @@
     </div>
 </section>
 
+<section class="instructores">
+    <div class="contenedor seccion">
+        <h2 class="text-center texto-primario">Nuestros Instructores</h2>
+        <p class="text-center">Instructores profesionales que te ayudarán a lograr tus objetivos</p>
+
+        <ul class="listado-instructores">
+            <?php
+                $args_instructores = array(
+                    'post_type' => 'instructores', 
+                    'posts_per_page' => 30
+                );
+                $instructores = new WP_Query($args_instructores);
+                while($instructores->have_posts()): $instructores->the_post();
+            ?>
+            <li class="instructor">
+                <?php the_post_thumbnail('mediano'); ?>
+                <div class="contenido text-center">
+                    <h3><?php the_title(); ?></h3>
+                    <?php the_content(); ?>
+
+                    <div class="especialidad">
+                        <?php
+                            $esp = get_field('especialidad');
+                            foreach($esp as $e): ?>
+                                <span class="etiqueta"><?php echo esc_html( $e ); ?></span>  
+                        
+                        <?php endforeach; ?>
+                    </div>
+                </div>
+            </li>
+
+            <?php endwhile; wp_reset_postdata(); ?>
+        </ul>
+
+    </div>
+</section>
+
 
 
 <?php get_footer(); ?>
